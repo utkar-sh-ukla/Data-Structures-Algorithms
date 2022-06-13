9cb9511ff9d7af4e108651658ff2ec0a05ef1b04
μtkarsh <61664827+utkar-sh-ukla@users.noreply.github.com>
2022-06-13 15:54:40 +0530

    Updated ll

--- Data Structures/Linked List/source.cpp
+++ Data Structures/Linked List/source.cpp
@@ -1,261 +1,211 @@
 #include <iostream>
-#include <memory>
-#include <string>
 
- /* @namespace data_structures */
-namespace data_structures {
+using namespace std;
 
-    /* @namespace linked_list */
-    namespace linked_list {
+struct node {
+    int data;
+    node* next;
+    node(int value) : data(value), next(NULL) {}
+} *head = NULL;
 
-        /*
-         * This function checks if the string passed consists of only digits.
-         * @param s To be checked if s contains only integers
-         * @returns true if there are only digits present in the string
-         * @returns false if any other character is found
-         */
-        bool isDigit(const std::string& s) {
-            // function statements here
-            for (char i : s) {
-                if (!isdigit(i)) {
-                    return false;
-                }
-            }
-            return true;
-        }
+bool Is_List_Empty() {
+    return (head == NULL);
+}
 
-        /*
-         * A link class containing a value and pointer to another link
-         */
-        class link {
-        private:
-            int pvalue;                   ///< value of the current link
-            std::shared_ptr<link> psucc;  ///< pointer to the next value on the list
+void Insert_At_Beginning(int value) {
+    node* temp = new node(value);
+    temp->next = head;
+    head = temp;
+}
 
-        public:
-            /**
-             * function returns the integer value stored in the link.
-             * @returns the integer value stored in the link.
-             */
-            int val() { return pvalue; }
+void Insert_At_End(int value) {
+    node* temp = new node(value);
+    if (Is_List_Empty()) {
+        head = temp;
+        return;
+    }
+    node* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = temp;
+}
 
-            /**
-             * function returns the pointer to next link
-             * @returns the pointer to the next link
-             * */
-            std::shared_ptr<link>& succ() { return psucc; }
+void Insert_After_Value(int desired, int value) {
 
-            /**
-             * Creates link with provided value and pointer to next link
-             * @param value is the integer stored in the link
-             */
-            explicit link(int value = 0) : pvalue(value), psucc(nullptr) {}
-        };
+    node* current = head;
+    while (current != NULL && current->data != desired)
+        current = current->next;
 
-        /**
-         * A list class containing a sequence of links
-         */
-        class list {
-        private:
-            std::shared_ptr<link> first;  ///< link before the actual first element
-            std::shared_ptr<link> last;   ///< last link on the list
-        public:
-            /**
-             * List constructor. Initializes the first and last link.
-             */
-            list() {
-                // Initialize the first link
-                first = std::make_shared<link>();
-                // Initialize the last link with the first link
-                last = nullptr;
-            }
+    if (current == NULL) {
+        cout << "Element " << desired << " is not in list" << endl;
+    }
+    else {
+        node* temp = new node(value);
+        temp->next = current->next;
+        current->next = temp;
+    }
+}
 
-            bool isEmpty();
+void Delete_At_Beginning() {
+    if (Is_List_Empty())
+        cout << "List is empty" << endl;
+    else {
+        node* temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+}
 
-            void push_back(int new_elem);
-            void push_front(int new_elem);
-            void erase(int old_elem);
-            void display();
-            std::shared_ptr<link> search(int find_elem);
-            void reverse();
-        };
+void Delete_At_End() {
+    if (Is_List_Empty()) {
+        cout << "List is empty" << endl;
+        return;
+    }
 
-        /**
-         * function checks if list is empty
-         * @returns true if list is empty
-         * @returns false if list is not empty
-         */
-        bool list::isEmpty() {
-            if (last == nullptr) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
+    if (head->next == NULL) {
+        delete head;
+        head = NULL;
+        return;
+    }
 
-        /**
-         * function adds new element to the end of the list
-         * @param new_elem to be added to the end of the list
-         */
-        void list::push_back(int new_elem) {
-            if (isEmpty()) {
-                first->succ() = std::make_shared<link>(new_elem);
-                last = first->succ();
-            }
-            else {
-                last->succ() = std::make_shared<link>(new_elem);
-                last = last->succ();
-            }
-        }
+    node* temp = head;
+    while (temp->next->next != NULL) {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = NULL;
+}
 
-        /**
-         * function adds new element to the beginning of the list
-         * @param new_elem to be added to front of the list
-         */
-        void list::push_front(int new_elem) {
-            if (isEmpty()) {
-                first->succ() = std::make_shared<link>(new_elem);
-                last = first->succ();
-            }
-            else {
-                std::shared_ptr<link> t = std::make_shared<link>(new_elem);
-                t->succ() = first->succ();
-                first->succ() = t;
-            }
-        }
+void Delete_With_Value(int desired) {
+    if (Is_List_Empty()) {
+        cout << "List is empty" << endl;
+        return;
+    }
 
-        /**
-         * function erases old element from the list
-         * @param old_elem to be erased from the list
-         */
-        void list::erase(int old_elem) {
-            if (isEmpty()) {
-                std::cout << "List is Empty!";
-                return;
-            }
-            std::shared_ptr<link> t = first;
-            std::shared_ptr<link> to_be_removed = nullptr;
-            while (t != last && t->succ()->val() != old_elem) {
-                t = t->succ();
-            }
-            if (t == last) {
-                std::cout << "Element not found\n";
-                return;
-            }
-            to_be_removed = t->succ();
-            t->succ() = t->succ()->succ();
-            to_be_removed.reset();
-            if (t->succ() == nullptr) {
-                last = t;
-            }
-            if (first == last) {
-                last = nullptr;
-            }
-        }
+    node* temp = head, * prev = NULL;
+    if (head->data == desired) {
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+        return;
+    }
 
-        /**
-         * function displays all the elements in the list
-         * @returns 'void'
-         */
-        void list::display() {
-            if (isEmpty()) {
-                std::cout << "List is Empty!";
-                return;
-            }
-            std::shared_ptr<link> t = first;
-            while (t->succ() != nullptr) {
-                std::cout << t->succ()->val() << "\t";
-                t = t->succ();
-            }
-        }
+    while (temp != NULL && temp->data != desired) {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        cout << "Element " << desired << " not in list" << endl;
+    }
+    else {
+        prev->next = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }
+}
 
-        /**
-         * function searchs for @param find_elem in the list
-         * @param find_elem to be searched for in the list
-         */
-        std::shared_ptr<link> list::search(int find_elem) {
-            if (isEmpty()) {
-                std::cout << "List is Empty!";
-                return nullptr;
-            }
-            std::shared_ptr<link> t = first;
-            while (t != last && t->succ()->val() != find_elem) {
-                t = t->succ();
-            }
-            if (t == last) {
-                std::cout << "Element not found\n";
-                return nullptr;
-            }
-            std::cout << "Element was found\n";
-            return t->succ();
-        }
-    }  // namespace linked_list
-}  // namespace data_structures
+void Search(int desired) {
+    node* temp = head;
+    while (temp != NULL && temp->data != desired) {
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        cout << "Element " << desired << " not found" << endl;
+    }
+    else {
+        cout << "Element " << desired << " is present in list" << endl;
+    }
+}
 
-/**
- * Main function:
- * Allows the user add and delete values from the list.
- * Also allows user to search for and display values in the list.
- * @returns 0 on exit
- */
+void Print_Linked_List() {
+    if (Is_List_Empty()) {
+        cout << "List is Empty" << endl;
+        return;
+    }
+
+    node* current = head;
+    while (current != NULL) {
+        cout << current->data;
+        current = current->next;
+        if (current != NULL)
+            cout << " -> ";
+    }
+    cout << endl;
+}
+
+void Length_Iterative() {
+    int length = 0;
+    node* temp = head;
+    while (temp) {
+        length += 1;
+        temp = temp->next;
+    }
+    cout << "Length is " << length << " (Iterative)" << endl;
+}
+
+int Recursive_Count(node* current) {
+    if (current == NULL)
+        return 0;
+    return (1 + Recursive_Count(current->next));
+}
+
+void Length_Recursive() {
+    int length = Recursive_Count(head);
+    cout << "Length is " << length << " (Recursive)" << endl;
+}
+
 int main() {
-    data_structures::linked_list::list l;
-    int choice = 0;
-    int x = 0;
-    std::string s;
-    do {
-        std::cout << "\n1. Insert";
-        std::cout << "\n2. Delete";
-        std::cout << "\n3. Search";
-        std::cout << "\n4. Print";
-        std::cout << "\n0. Exit";
-        std::cout << "\n\nEnter you choice : ";
-        std::cin >> choice;
-        switch (choice) {
-        case 1:
-            std::cout << "\nEnter the element to be inserted : ";
-            std::cin >> s;
+    for (int i = 0; i < 5; i++)
+        Insert_At_Beginning(i);
 
-            if (data_structures::linked_list::isDigit(s)) {
-                x = std::stoi(s);
-                l.push_back(x);
-            }
-            else {
-                std::cout << "Wrong Input!\n";
-            }
-            break;
-        case 2:
-            std::cout << "\nEnter the element to be removed : ";
-            std::cin >> s;
-            if (data_structures::linked_list::isDigit(s)) {
-                x = std::stoi(s);
-                l.erase(x);
-            }
-            else {
-                std::cout << "Wrong Input!\n";
-            }
-            break;
-        case 3:
-            std::cout << "\nEnter the element to be searched : ";
-            std::cin >> s;
-            if (data_structures::linked_list::isDigit(s)) {
-                x = std::stoi(s);
-                std::shared_ptr<data_structures::linked_list::link> found =
-                    l.search(x);
-            }
-            else {
-                std::cout << "Wrong Input!\n";
-            }
-            break;
-        case 4:
-            l.display();
-            std::cout << "\n";
-            break;
-        default:
-            std::cout << "Invalid Input\n" << std::endl;
-            break;
-        }
-    } while (choice != 0);
+    Print_Linked_List();
+
+    for (int i = 5; i < 10; i++)
+        Insert_At_End(i);
+
+    Print_Linked_List();
+
+    Insert_After_Value(5, 9);
+    Insert_After_Value(10, 9);
+
+    Print_Linked_List();
+
+    for (int i = 0; i < 3; i++)
+        Delete_At_End();
+
+    Print_Linked_List();
+
+    for (int i = 0; i < 3; i++)
+        Delete_At_Beginning();
+
+    Print_Linked_List();
+
+    Delete_With_Value(1);
+    Delete_With_Value(5);
+
+    Print_Linked_List();
+
+    Search(6);
+    Search(8);
+
+    Length_Iterative();
+    Length_Recursive();
+
     return 0;
 }
+
+/*  Output
+    4 -> 3 -> 2 -> 1 -> 0
+    4 -> 3 -> 2 -> 1 -> 0 -> 5 -> 6 -> 7 -> 8 -> 9
+    Element 10 is not in list
+    4 -> 3 -> 2 -> 1 -> 0 -> 5 -> 9 -> 6 -> 7 -> 8 -> 9
+    4 -> 3 -> 2 -> 1 -> 0 -> 5 -> 9 -> 6
+    1 -> 0 -> 5 -> 9 -> 6
+    0 -> 9 -> 6
+    Element 6 is present in list
+    Element 8 not found
+    Length is 3 (Iterative)
+    Length is 3 (Recursive)
+*/
